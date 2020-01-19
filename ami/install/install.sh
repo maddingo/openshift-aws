dcbb691abbb3ddd52939fc1d767535b9580823d0
Martin Goldhahn <mgoldhahn@gmail.com>
2020-01-20 00:38:42 +0100

    install tools in AMI

--- ami/install/install.sh
+++ ami/install/install.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
+# See https://github.com/arunvdsharma/openshift-centos/blob/master/install-tools.sh
 
-sudo yum install -y git
+## Default variables to use
+
+# install updates
+sudo yum update -y
+
+# install the following base packages
+sudo yum install -y  wget git net-tools docker-1.13.1\
+				bind-utils iptables-services \
+				bridge-utils \
+				kexec-tools sos psacct openssl-devel \
+				httpd-tools NetworkManager \
+				python-cryptography python2-pip python-devel python-passlib
+
+#install epel
+sudo yum -y install epel-release
+
+# Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
+sudo sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+
+sudo systemctl | grep "NetworkManager.*running"
+if [ $? -eq 1 ]; then
+	sudo systemctl start NetworkManager
+	sudo systemctl enable NetworkManager
+fi
+
+# install the packages for Ansible
+sudo yum -y --enablerepo=epel install pyOpenSSL
+
+#curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+#sudo yum -y --enablerepo=epel install ansible.rpm
+
+sudo yum -y --enablerepo=epel install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
